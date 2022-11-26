2745bd6fde8abd04e52b2eb5a099c67990969bb1
nicolas rolland <nrolland@users.noreply.github.com>
2022-11-26 12:01:01 +0100

    initial

---
+++ docusaurus.config.js
@@ -0,0 +1,122 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+	title: "Nuzletter Dev",
+	tagline: "Nuzletter",
+	url: "https://dev.nuzletter.com",
+	baseUrl: "/",
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
+	favicon: "img/favicon.ico",
+
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: "nuzletter", // Usually your GitHub org/user name.
+	projectName: "dev", // Usually your repo name.
+
+	// Even if you don't use internalization, you can use this field to set useful
+	// metadata like html lang. For example, if your site is Chinese, you may want
+	// to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en"],
+	},
+
+	presets: [
+		[
+			"classic",
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					sidebarPath: require.resolve("./sidebars.js"),
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl: "https://github.com/nuzletter/dev.nuzletter.com/edit/main/",
+				},
+				blog: {
+					showReadingTime: true,
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl: "https://github.com/nuzletter/dev.nuzletter.com/edit/main/",
+				},
+				theme: {
+					customCss: require.resolve("./src/css/custom.css"),
+				},
+			}),
+		],
+	],
+
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			navbar: {
+				title: "Nuzletter Dev",
+				logo: {
+					alt: "Nuzletter Dev Logo",
+					src: "img/logo.svg",
+				},
+				items: [
+					{
+						type: "doc",
+						docId: "intro",
+						position: "left",
+						label: "Docs for devs",
+					},
+					{ to: "/blog", label: "Blog", position: "left" },
+					{
+						href: "https://github.com/nuzletter/dev.nuzletter.com",
+						label: "GitHub",
+						position: "right",
+					},
+				],
+			},
+			footer: {
+				style: "dark",
+				links: [
+					{
+						title: "Nuzletter Dev",
+						items: [
+							{
+								label: "Tutorial",
+								to: "/docs/intro",
+							},
+						],
+					},
+					{
+						title: "Community",
+						items: [
+							{
+								label: "Twitter",
+								href: "https://twitter.com/Nuzletter_com",
+							},
+						],
+					},
+					{
+						title: "More",
+						items: [
+							{
+								label: "Blog",
+								to: "/blog",
+							},
+							{
+								label: "GitHub",
+								href: "https://github.com/nuzletter/",
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} Nuzletter, Inc.. Built with Docusaurus.`,
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+			},
+		}),
+};
+
+module.exports = config;
