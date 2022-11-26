07947fb4da072cd8eeba57c81b49c0d49752af87
nicolas rolland <nrolland@users.noreply.github.com>
2022-11-26 13:01:10 +0100

    asd

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
 	title: "Nuzletter Dev",
-	tagline: "Nuzletter",
+	tagline: "Nuzletter tagline",
 	url: "https://dev.nuzletter.com",
 	baseUrl: "/",
 	onBrokenLinks: "throw",
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Nuzletter Tutorial - 5min ⏱️
+            Nuzletter Tutorial
           </Link>
         </div>
       </div>
